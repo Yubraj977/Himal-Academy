f482075645ef31b2274b9f5be31ed923ca24d6ed
Yubraj977 <yubrajkhatri977@gmail.com>
2024-06-02 13:36:04 +0545

    added things

--- src/pages/Home/AboutHome.jsx
+++ src/pages/Home/AboutHome.jsx
@@ -13,7 +13,7 @@ const AboutHome = () => {
                         <span className="font-bold text-4xl text-cyan-700">HIMAL ACADEMY</span>
                     </h1>
 
-                    <p className=" font-medium mt-10">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium quia inventore ad mollitia sed quidem magnam dolore deleniti consequuntur neque molestias fugiat quisquam, voluptates voluptatum, vel autem blanditiis, repellendus esse.
+                    <p className=" font-medium mt-4 lg:mt-10">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium quia inventore ad mollitia sed quidem magnam dolore deleniti consequuntur neque molestias fugiat quisquam, voluptates voluptatum, vel autem blanditiis, repellendus esse.
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat ex beatae autem voluptatem eaque? Repellat placeat ad vitae quia! Quae consequatur, sed ullam quia laboriosam corrupti iure. Facilis, cumque voluptatibus?
 
                     </p>
@@ -21,21 +21,25 @@ const AboutHome = () => {
             </div>
 
 
-            <div className="flex my-10 lg:px-side gap-8 flex-col lg:flex-row px-mside">
+            <div className="flex my-10 lg:px-side gap-8 flex-col lg:flex-row px-mside mt-20" >
                 <div className="right lg:w-1/2">
                     <h1 className="flex flex-col">
-                        <span className="text-3xl font-bold">Welcome to</span>
+                        <span className="text-3xl font-bold"> Explore</span>
                         <span className="font-bold text-4xl text-cyan-700">HIMAL ACADEMY</span>
                     </h1>
 
-                    <p className=" font-medium mt-10">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium quia inventore ad mollitia sed quidem magnam dolore deleniti consequuntur neque molestias fugiat quisquam, voluptates voluptatum, vel autem blanditiis, repellendus esse.
+                    <p className=" font-medium mt-4 lg:mt-10">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium quia inventore ad mollitia sed quidem magnam dolore deleniti consequuntur neque molestias fugiat quisquam, voluptates voluptatum, vel autem blanditiis, repellendus esse.
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat ex beatae autem voluptatem eaque? Repellat placeat ad vitae quia! Quae consequatur, sed ullam quia laboriosam corrupti iure. Facilis, cumque voluptatibus?
 
                     </p>
                 </div>
-                <div className="left border h-full lg:w-1/2">
-                    <img src="https://images.unsplash.com/photo-1514369118554-e20d93546b30?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="h-full w-full object-cover" alt="the section" />
-                </div>
+                <video class="h-full  rounded-lg lg:w-1/2 w-full" controls>
+                    <source
+                        src="https://docs.material-tailwind.com/demo.mp4"
+                        type="video/mp4"
+                    />
+                    Your browser does not support the video tag.
+                </video>
 
 
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,23 +3,25 @@ import himalb from '/himalb.jpeg'
 import MySlider from './slider/MySlider'
 import Nav from '../Outline/Nav'
 import AboutHome from './AboutHome'
+import HomeBlogs from './HomeBlogs'
 function Home() {
+ 
   return (
     <div className=''>
       <MySlider />
-      <AboutHome/>
+      <AboutHome />
       <div className="lg:px-side px-mb_side flex justify-center flex-col items-center py-24 bg-cyan-700 w-full">
-      <h1 className="text-white text-4xl text-center font-semibold">JOIN WITH US</h1>
-      <p className="text-white text-xl mt-3 font-normal text-center">Join as fast as you can , Fast application will get greater advantages. </p>
-      <div className="form-email w-full mt-3 flex  lg:w-[55%]">
-        <button className='bg-red-600 hover:bg-red-900 w-full px-4 py-2 rounded-sm text-white font-semibold'>Apply Now</button>
-      
-      </div>
-    </div>
+        <h1 className="text-white text-4xl text-center font-semibold">JOIN WITH US</h1>
+        <p className="text-white text-xl mt-3 font-normal text-center">Join as fast as you can , Fast application will get greater advantages. </p>
+        <div className="form-email w-full mt-3 flex  lg:w-[55%]">
+          <button className='bg-red-600 hover:bg-red-900 w-full px-4 py-2 rounded-sm text-white font-semibold'>Apply Now</button>
 
-    <div className='flex flex-col items-center'>
-      <h1 className='font-bold text-red-900 text-4xl text-center'>Lates News Programs & articles</h1>
-    </div>
+        </div>
+      </div>
+     
+        <HomeBlogs />
+    
+
     </div>
 
   )
